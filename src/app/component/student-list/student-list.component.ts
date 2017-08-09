dfba4917cec37b62b3ff331ff48d119ccf8e3d50
Mehta Sudesh <Mehta@iazi.ch>
2017-08-09 04:39:30 +0200

    sess 14 ass 2

---
+++ src/app/component/student-list/student-list.component.ts
@@ -0,0 +1,29 @@
+/* Student List */
+import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs/Observable";
+/* interfaces */
+import { IStudentList } from 'app/interface/student-list';
+/* services */
+import { StudentService } from 'app/service/Student.service';
+
+@Component({
+  selector: 'app-student-list',
+  templateUrl: './student-list.component.html',
+  styleUrls: ['./student-list.component.css'],
+  providers: []
+})
+export class StudentListComponent implements OnInit {
+
+  StudentsArray: IStudentList[] = [];
+  //StudentsArray: Observable<IStudentList[]>;
+
+  constructor(private _student_Service: StudentService) {
+  }
+
+  ngOnInit() {
+    this._student_Service.getStudentList().subscribe((response)=>{this.StudentsArray=response});
+    console.log("list service");
+    console.log(this.StudentsArray);
+  }
+
+}
